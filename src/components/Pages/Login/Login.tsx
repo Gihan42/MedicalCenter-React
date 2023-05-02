40dcd1299fa097d3411c8ef47ba75597ae9d8853
Gihan Madushanka <gihanm729@gmail.com>
2023-05-03 01:11:35 +0530

    updated

--- src/components/Pages/Login/Login.tsx
+++ src/components/Pages/Login/Login.tsx
@@ -1,13 +1,16 @@
 import React, { Component } from 'react'
 import backgroundImage from '../../../assets/medium-shot-doctors-wearing-protective-equipment.jpg'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
 
 export default class Login extends Component {
   render() {
     return (
       <>
         <div className='h-screen bg-cover flex justify-center items-center' style={{ backgroundImage: `url(${backgroundImage})` }}>
-            <div className='border-2 border-gray-500  w-1/2 h-96 rounded-3xl shadow-gray-500 shadow-lg hover:drop-shadow-xl backdrop-blur-sm flex justify-center '>
-                
+            <div className='border-2 border-gray-500  w-1/2 h-96 rounded-3xl shadow-gray-500 shadow-lg hover:drop-shadow-xl backdrop-blur-sm flex justify-center space-x-5 p-10'>
+                <h2 className='text-4xl '>INTENSIFYING <span>  </span></h2> <h2 className='text-4xl '> LIFE <span>  </span></h2><h2 className='text-4xl '> AND </h2><h2 className='text-4xl '>CARE</h2>
             </div>
         </div>
        </>
