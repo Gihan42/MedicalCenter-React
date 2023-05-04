230ed12dbacf9728c047d45bbae9883efe725ef7
Gihan Madushanka <gihanm729@gmail.com>
2023-05-04 14:26:53 +0530

    updated

--- src/Pages/Contact/Contact.tsx
+++ src/Pages/Contact/Contact.tsx
@@ -1,8 +1,9 @@
 import React, { Component } from 'react'
 import Header from '../../components/Header/Header'
 import  Title  from '../../components/Title/Title'
-import TextField from '@mui/material/TextField'
-
+import TextField from '@mui/material/TextField';
+import AddIcCallIcon from '@mui/icons-material/AddIcCall';
+import AddLocationIcon from '@mui/icons-material/AddLocation';
 export default class Contact extends Component {
   render() {
     return (
@@ -11,17 +12,44 @@ export default class Contact extends Component {
             <div className='pb-20 bg-gradient-to-r from-blue-500 h-screen'>
             
               <div className="grid grid-cols-2 gap-4 p-36  h-screen ">
-                <div className=" p-10 space-y-5 justify-center ">
+                <div className="  space-y-5 justify-center ">
                       <h1 className='text-6xl text-slate-900'>Contact Us</h1>
-                      <section className=' p-5 flex border-2 border-white h-5/6'>
-                      <form>
-                          <div  className='flex justify-start space-x-5'>
-                          <TextField id="standard-basic" 
-                          label="Standard" 
-                          variant="standard" />
+                      <section className=' p-5 flex rounded-2xl h-auto shadow-2xl shadow-black bg-white border-2 border-white '>
+                      <form className='w-full flex flex-col space-y-3 mt-3'>
+                          <div  className='flex justify-start space-x-5 text-2xl '>
+                            <AddIcCallIcon className='text-slate-800' style={{fontSize: "48px"}}/>
+                              <h2 className='text-3xl font-bold text-cyan-900'>Phone</h2>
                           </div>
-                          <div className=''></div>
-                          <div className=''></div>
+                          <h2 className='text-2xl font-bold text-emerald-900 pl-16 relative bottom-8 space-x-5'>+971-2345678 , +971-2345679 </h2>
+                          
+                          <div  className='flex justify-start space-x-5 text-2xl '>
+                            <AddLocationIcon className='text-slate-800 ' style={{fontSize: "48px"}}/>
+                              <h2 className='text-3xl font-bold text-cyan-900'>Address</h2>
+                          </div>
+                          <h2 className='text-2xl font-bold text-emerald-900 pl-16 relative bottom-8 space-x-5'>No 456,GalleRoad,Matara </h2>
+                          
+                          <div  className='flex justify-start space-x-5 text-2xl '>
+                            <AddLocationIcon className='text-slate-800 ' style={{fontSize: "48px"}}/>
+                              <h2 className='text-3xl font-bold text-cyan-900'>Email</h2>
+                          </div>
+                          <h2 className='text-2xl font-bold text-emerald-900 pl-16 relative bottom-8 space-x-5'>araliyamcenter@gmail.com</h2>
+                           
+                          <TextField
+                            label="Type Message Here"
+                            type="text"
+                            variant="outlined"
+                            name="description"
+                            placeholder="send message"
+                            fullWidth={true}
+                            multiline
+                            minRows={5}
+                            maxRows={Infinity}
+                            required
+                          />
+                           <button className="py-2 bg-slate-800 text-white rounded">
+                            <h6>send message</h6>
+                          </button>
+                      
                       </form>
                       </section>
                   </div>
