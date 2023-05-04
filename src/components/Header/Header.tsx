a1d5491f605272698127a3f2f3e6f43957495040
Gihan Madushanka <gihanm729@gmail.com>
2023-05-05 00:11:01 +0530

    updated

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,27 +4,25 @@ import LogoutIcon from '@mui/icons-material/Logout';
 
 export default class Header extends Component {
   render() {
-    const navStyles="font-bold border-b-2";
+    const navStyles="text-slate-900";
 
     return (
         <div className='bg-transparent h-20 p-4 w-full fixed top-0 z-10 '>
-        <div className=' w-full h-full	'>
+        <div className=' w-full h-full '>
             <div className='flex w-full h-full items-center'>
                 <div className='w-1/2'>
-                    <div className='font-Poppins font-bold text-slate-900 text-4xl	'>Araliya Medical-Center</div>
+                    <div className='font-Poppins font-bold text-slate-900 text-4xl '>Araliya Medical-Center</div>
                 </div>
                 <div className='w-1/2'>
-                    <div className='flex w-full justify-end gap-12 '>
+                    <div className='flex w-full justify-end gap-12 text-2xl text-slate-900 font-bold '>
                       
-                        <NavLink to={"/home"} className= 'cursor-pointer text-slate-900 pr-0 font-bold text-2xl'>Home</NavLink>
-                        <NavLink to={"/chanelling"} className='text-slate-900 font-bold text-2xl'>Chanelling</NavLink>
-                        <NavLink to={"/doctors"} className='text-slate-900 font-bold text-2xl'>Doctors</NavLink>
-                        <NavLink to={"/contact"} className='text-slate-900 font-bold text-2xl'>Contact</NavLink>
-                        <NavLink to={"/"} className='text-slate-900 font-bold text-2xl'><LogoutIcon/></NavLink>
-
+                        <NavLink to={"/home"} className={({isActive})=>(isActive? navStyles:"text-slate-900 decoration-transparent")}>Home</NavLink>
+                        <NavLink to={"/chanelling"} className={({isActive})=>(isActive? navStyles:"text-slate-900 decoration-transparent ")}>Chanelling</NavLink>
+                        <NavLink to={"/doctors"} className={({isActive})=>(isActive? navStyles:'text-slate-900 decoration-transparent')}>Doctors</NavLink>
+                        <NavLink to={"/contact"} className={({isActive})=>(isActive? navStyles:'text-slate-900 decoration-transparent')}>Contact</NavLink>
+                        <NavLink to={"/"} className={({isActive})=>(isActive? navStyles:'text-slate-900 decoration-transparent')}><LogoutIcon/></NavLink>
 
                     </div>
-
                 </div>
             </div>
         </div>
